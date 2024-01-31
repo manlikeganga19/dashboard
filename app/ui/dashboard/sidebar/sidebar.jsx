fa8bd1d7f9a02003853fb4d1e705c8f93144d888
Daniel <owenjodaniel19@gmail.com>
2024-01-31 20:46:57 +0300

    sidebar layout

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./sidebar.module.css";
+import MenuLink from "./menuLink/menuLink";
+import Image from "next/image";
+
 import {
   MdDashboard,
   MdSupervisedUserCircle,
@@ -77,7 +80,27 @@ const menuItems = [
 ];
 
 const sidebar = () => {
-  return <div className={styles.container}>sidebar</div>;
+  return (
+    <div className={styles.container}>
+    <div className={styles.user}>
+      <Image className={styles.userImage} src="/noavatar.png" alt="" width="50" height="50"/>
+      <div className={styles.userDetail}>
+        <span className={styles.username}>Daniel Oganga</span>
+        <span className={styles.userTitle}> Administrator</span>
+      </div>
+    </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map(item => (
+              <MenuLink item={item} key={item.title}/>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default sidebar;
