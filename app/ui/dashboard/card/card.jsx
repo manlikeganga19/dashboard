cb35fab02aca76f7331bf2e782172a3e469771e9
Daniel <owenjodaniel19@gmail.com>
2024-02-01 12:57:17 +0300

    display card

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./card.module.css";
+import { MdSupervisedUserCircle } from "react-icons/md";
 
 const card = () => {
-  return <div className={styles.container}>card</div>;
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle sixe={24} />
+      <div className={styles.texts}>
+        <span className={styles.tilte}>Total users</span>
+        <span className={styles.number}> 10000</span>
+        <span clasName={styles.detail}>
+          <span className={styles.positive}>12%</span> more than the previous
+          week
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default card;
