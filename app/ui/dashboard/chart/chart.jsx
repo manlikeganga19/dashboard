211e429b13ffc18f17ac8d94e64717c02d1f1ac3
Daniel <owenjodaniel19@gmail.com>
2024-02-01 16:23:25 +0300

    live charts

--- app/ui/dashboard/chart/chart.jsx
+++ app/ui/dashboard/chart/chart.jsx
@@ -1,7 +1,14 @@
+"use client";
+
 import React from "react";
 import styles from "./chart.module.css";
+
 const chart = () => {
-  return <div className={styles.container}>chart</div>;
+  return (
+    <div className={styles.container}>
+      <h2>Weekly Recap</h2>
+    </div>
+  );
 };
 
 export default chart;
