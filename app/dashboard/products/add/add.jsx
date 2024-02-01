627f0be420634ee8a972956ca27111f3a27959a5
Daniel <owenjodaniel19@gmail.com>
2024-02-01 18:27:24 +0300

    poducts page anf form

---
+++ app/dashboard/products/add/add.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./styles.module.css";
+const add = () => {
+  return <div className={styles.container}> addProductspage</div>;
+};
+
+export default add;
