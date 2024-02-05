c5af43ca8c699dcd6ba49feb425e4c15715950dd
Daniel <owenjodaniel19@gmail.com>
2024-02-05 16:16:37 +0300

    login page

---
+++ app/ui/login/loginForm/loginForm.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import styles from "./loginForm.module.css";
+
+const loginForm = () => {
+  return (
+    <form action="" className={styles.form}>
+      <h1>Login</h1>
+      <input type="text" placeholder="username" name="username" />
+      <input type="password" placeholder="password" name="password" />
+      <button>Login</button>
+      {/* {state && state} */}
+    </form>
+  );
+};
+
+export default loginForm;
